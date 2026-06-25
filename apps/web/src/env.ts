b3502d6d6a28fd016fba3127b7510442190130cc
prudentbird <prudentbird@gmail.com>
2026-06-25 16:23:12 +0100

    chore: make PostHog environment variables optional and conditionally execute sourcemap upload during postbuild

--- apps/web/src/env.ts
+++ apps/web/src/env.ts
@@ -6,8 +6,8 @@ export const env = createEnv({
     NODE_ENV: z
       .enum(["local", "test", "development", "staging", "production"])
       .default("development"),
-    POSTHOG_CLI_ENV_ID: z.string(),
-    POSTHOG_CLI_API_KEY: z.string(),
+    POSTHOG_CLI_ENV_ID: z.string().optional(),
+    POSTHOG_CLI_API_KEY: z.string().optional(),
   },
   client: {
     NEXT_PUBLIC_POSTHOG_KEY: z.string().startsWith("phc_"),
